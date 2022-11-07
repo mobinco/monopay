455484b5e3d8d339cec7c340b6a6d4b6d044c169
Mobin Soft <mobinsmtp@gmail.com>
2022-11-07 09:52:44 +0330

    release

--- src/payment-info.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class PaymentInfo {
-    readonly referenceId: string | number;
-    readonly method: 'GET' | 'POST';
-    readonly url: string;
-    readonly params: Record<string, any>;
-    constructor(referenceId: string | number, method: 'GET' | 'POST', url: string, params?: Record<string, any>);
-    getScript(): string;
-}
